357eac810acdda10d500a6c3255c58ac141dc6ac
Saeed Zare <44446617+MSaeed1381@users.noreply.github.com>
2023-04-02 07:22:43 +0330

    add error message

--- simple-login-form/script.js
+++ simple-login-form/script.js
@@ -2,7 +2,7 @@ let usernameInput = document.getElementById('username');
 let passwordInput = document.getElementById('password');
 let modal = document.getElementsByClassName('modal-massage')[0]
 function validate(){
-    if (passwordInput.value.length > 8 && usernameInput.value.length > 12){
+    if (passwordInput.value.length >= 8 && usernameInput.value.length >= 12){
         modal.style.backgroundColor = 'blue'
         modal.innerText = 'you logged in :))'
         modal.style.display = 'block';
@@ -10,5 +10,29 @@ function validate(){
         modal.style.backgroundColor = 'red'
         modal.innerText = 'please correct your inputs :('
         modal.style.display = 'block';
+    }
+
+    setTimeout(function (){
+        modal.style.display = 'none';
+    }, 3000)
+}
+
+function usernameKeyPress(){
+    let errorMessage = document.getElementsByClassName('error-message')[0]
+    if (usernameInput.value.length < 11){
+        errorMessage.innerText = "invalid password (at least 12 characters";
+    } else {
+        errorMessage.innerText = "";
+
+    }
+}
+
+function passwordKeyPress(){
+    let errorMessage = document.getElementsByClassName('error-message')[1]
+    if (passwordInput.value.length < 7){
+        errorMessage.innerText = "invalid password (at least 8 characters)";
+    } else {
+        errorMessage.innerText = "";
+
     }
 }
