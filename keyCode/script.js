a8ffb64eb77827b615266f2a31e8ca7eeaa6ed90
Saeed Zare <44446617+MSaeed1381@users.noreply.github.com>
2023-04-05 18:28:09 +0330

    complete the project

--- keyCode/script.js
+++ keyCode/script.js
@@ -4,17 +4,25 @@ let placeHolderBody = $.querySelector('.main-body__content');
 let mainTitle = $.querySelector('.main-body__title');
 let informationBoxTitle = $.querySelectorAll('.information-box__title');
 let informationBoxContent = $.querySelectorAll('.information-box__content');
-let values = {
+let bigNumber = $.querySelector('.big-number');
+let informationWrapper = $.querySelector('.information');
 
-};
 $.body.addEventListener('keydown', function (event){
     event.preventDefault();
     mainTitle.innerHTML = `Java Script Key Code ${event.keyCode}`
     placeHolderBody.style.display = 'none';
-    values['event.key'] = event.key;
-    values['event.location'] = event.location;
-    values['event.code'] = event.code;
-    values['event.which'] = event.which;
+    informationWrapper.style.display = 'flex';
+    bigNumber.style.display = 'inline';
+    bigNumber.innerHTML = String(event.keyCode);
+    let values = {
+        'event.key': event.key,
+        'event.location': event.location,
+        'event.code': event.code,
+        'event.which': event.which,
+        'event.isTrusted': event.isTrusted,
+        'event.isComposing': event.isComposing,
+    };
+
     for (let i = 0; i < informationBoxTitle.length; i++) {
         console.log(Object.keys(values)[i])
         informationBoxTitle[i].innerHTML = Object.keys(values)[i];
